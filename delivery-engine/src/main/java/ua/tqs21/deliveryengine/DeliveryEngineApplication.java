2794d6ca2de6bb7ff488bac06ca0848474de07c6
VascoRegal <vascoregal24@gmail.com>
2022-05-23 23:04:51 +0100

    init delivery backend

---
+++ delivery-engine/src/main/java/ua/tqs21/deliveryengine/DeliveryEngineApplication.java
@@ -0,0 +1,13 @@
+package ua.tqs21.deliveryengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryEngineApplication.class, args);
+	}
+
+}
