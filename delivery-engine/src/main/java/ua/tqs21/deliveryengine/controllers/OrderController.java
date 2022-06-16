ae8994dd0a800cf259ea3b540988526e0904b2d7
VascoRegal <vascoregal24@gmail.com>
2022-06-17 00:27:45 +0100

    order status management

--- delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/OrderController.java
+++ delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/OrderController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.tqs21.deliveryengine.dto.OrderPostDTO;
+import ua.tqs21.deliveryengine.enums.OrdStatus;
 import ua.tqs21.deliveryengine.models.Order;
 import ua.tqs21.deliveryengine.services.OrderService;
 
@@ -30,5 +32,25 @@ public class OrderController {
     public List<Order> getOrders() {
         System.out.println("entrou");
         throw new NotYetImplementedException();
+    }
+
+    @PostMapping("/{id}/accept")
+    public Order acceptOrder(@PathVariable("id")  int id) {
+        return this.orderService.accept(id);
+    }
+
+    @PostMapping("/{id}/collect")
+    public Order collectOrder(@PathVariable("id")  int id) {
+        return this.orderService.collect(id);
+    }
+
+    @PostMapping("/{id}/deliver")
+    public Order deliverOrder(@PathVariable("id")  int id) {
+        return this.orderService.deliver(id);
+    }
+
+    @PostMapping("/{id}/cancel")
+    public Order cancelOrder(@PathVariable("id")  int id) {
+        return this.orderService.cancel(id);
     }
 }
