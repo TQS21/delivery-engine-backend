b58b1a139207dc5a3e7bd0ea6621b41dc0a1773a
VascoRegal <vascoregal24@gmail.com>
2022-06-20 05:50:08 +0100

    swagger docs

--- delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/OrderController.java
+++ delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/OrderController.java
@@ -13,6 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import ua.tqs21.deliveryengine.dto.OrderPostDTO;
 import ua.tqs21.deliveryengine.models.Address;
 import ua.tqs21.deliveryengine.models.Order;
@@ -25,48 +33,122 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Operation(summary = "Create a new Order")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Created Order",
+                    content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Order.class)))
+    })
     @PostMapping("/")
-    public Order postOrderFromDto(@RequestBody OrderPostDTO order) throws URISyntaxException, ParseException, IOException {
+    public Order postOrderFromDto(
+        
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Order information", required = true, content = @Content(schema = @Schema(implementation = OrderPostDTO.class)))
+        @RequestBody OrderPostDTO order) throws URISyntaxException, ParseException, IOException {
         return orderService.createOrderFromDTO(order);
     }
 
+    @Operation(summary = "Get All Orders")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order List",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Order.class))))})
     @GetMapping("/")
     public List<Order> getOrders() {
         return orderService.getAllOrders();
     }
 
+    @Operation(summary = "Get Order by Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "404", description = "Order Not Found",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
-    public Order getOrder(@PathVariable int id) {
+    public Order getOrder(
+        @Parameter(name = "id", description = "Order's Id")
+        @PathVariable int id) {
         return this.orderService.getOrderById(id);
     }
 
+    @Operation(summary = "Get All Active Orders")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Active Order List",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Order.class))))})
     @GetMapping("/active")
     public List<Order> getActiveOrders() {
         return this.orderService.getActiveOrders();
     }
 
-    @GetMapping("/nearby")
-    public List<Order> getNearbyOrders(@RequestBody Address source) throws Exception {
+
+    @Operation(summary = "Get All Nearby Orders (30km Range)")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Nearby Order List",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Order.class)))),
+                            @ApiResponse(responseCode = "400", description = "Invalid Address",
+                            content = @Content(mediaType = "application/json"))
+                })
+    @PostMapping("/nearby")
+    public List<Order> getNearbyOrders(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Current Rider Address", required = true, content = @Content(schema = @Schema(implementation = Address.class)))
+        @RequestBody Address source) throws Exception {
         return orderService.getNearbyOrders(source);
     }
 
+
+    @Operation(summary = "Accept Order of Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "404", description = "Order Not Found",
+                    content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Invalid Order status transition",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class)))
+    })
     @PostMapping("/{id}/accept")
-    public Order acceptOrder(@PathVariable("id")  int id) {
+    public Order acceptOrder(
+        @Parameter(name = "id", description = "Order's Id")
+        @PathVariable("id")  int id) {
         return this.orderService.accept(id);
     }
 
+
+    @Operation(summary = "Collect Order of Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "404", description = "Order Not Found",
+                    content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Invalid Order status transition",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class)))
+    })
     @PostMapping("/{id}/collect")
-    public Order collectOrder(@PathVariable("id")  int id) {
+    public Order collectOrder(
+        @Parameter(name = "id", description = "Order's Id")
+        @PathVariable("id")  int id) {
         return this.orderService.collect(id);
     }
 
+
+    @Operation(summary = "Deliver Order of Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "404", description = "Order Not Found",
+                    content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Invalid Order status transition",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class)))
+    })
     @PostMapping("/{id}/deliver")
-    public Order deliverOrder(@PathVariable("id")  int id) {
+    public Order deliverOrder(
+        @Parameter(name = "id", description = "Order's Id")
+        @PathVariable("id")  int id) {
         return this.orderService.deliver(id);
     }
 
+
+    @Operation(summary = "Cancel Order of Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Order with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "404", description = "Order Not Found",
+                    content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Invalid Order status transition",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class)))
+    })
     @PostMapping("/{id}/cancel")
-    public Order cancelOrder(@PathVariable("id")  int id) {
+    public Order cancelOrder(
+        @Parameter(name = "id", description = "Order's Id")
+        @PathVariable("id")  int id) {
         return this.orderService.cancel(id);
     }
 }
--- delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/ServiceController.java
+++ delivery-engine/src/main/java/ua/tqs21/deliveryengine/controllers/ServiceController.java
@@ -14,6 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import ua.tqs21.deliveryengine.dto.ServicePostDTO;
 import ua.tqs21.deliveryengine.models.Service;
 import ua.tqs21.deliveryengine.services.ServiceService;
@@ -24,18 +32,37 @@ public class ServiceController {
     @Autowired
     private ServiceService serviceService;
 
+    @Operation(summary = "Register a new Shop")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Created Shop",
+                    content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Service.class))),
+                    @ApiResponse(responseCode = "400", description = "Email already in use",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/")
-    public Service postService(@RequestBody ServicePostDTO service) throws URISyntaxException, ParseException, IOException {
+    public Service postService(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User information", required = true, content = @Content(schema = @Schema(implementation = ServicePostDTO.class)))
+        @RequestBody ServicePostDTO service) throws URISyntaxException, ParseException, IOException {
         return serviceService.createServiceFromDTO(service);
     }
 
+    @Operation(summary = "Get All Shops")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Shop List",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Service.class))))})
     @GetMapping("/")
     public List<Service> getServices() {
         return serviceService.getAll();
     }
 
+    @Operation(summary = "Get Shop by Id")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Shop with Id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Service.class))),
+                    @ApiResponse(responseCode = "404", description = "Shop Not Found",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
-    public Service getService(@PathVariable("id") int id) {
+    public Service getService(
+        @Parameter(name = "id", description = "Admin's Id")
+        @PathVariable("id") int id) {
         return serviceService.getById(id);
     }
 
